57d621ac48b18587d4862cf1a8af6674e7a51b20
Kei-7777 <69491052+Kei-7777@users.noreply.github.com>
2021-04-12 07:13:07 +0900

    Team処理を追加+若干修正

--- src/main/java/net/kunmc/lab/angrybird/team/TeamManager.java
+++ src/main/java/net/kunmc/lab/angrybird/team/TeamManager.java
@@ -22,4 +22,26 @@ public class TeamManager {
     public boolean isLeader(Player p){
         return p.getUniqueId().equals(team1_leader) || p.getUniqueId().equals(team2_leader);
     }
+    
+    public boolean isExists(Player p){
+    return team1_players.contains(p.getUniqueId()) || team2_players.contains(p.getUniqueId()); 
+    }
+    
+    public void remove(Player p){
+    team1_players.remove(p.getUniqueId());
+    team2_players.remove(p.getUniqueId());
+    }
+    
+    public void reset(int id){
+    if(id == 1 || id == 2){
+    getById(id).clear();
+    } else {
+    System.out.println("Invalid team id " + id);
+    }
+    
+    public List<UUID> getById(int id){
+    if(id == 1) return team1_players;
+    if(id == 2) return team2_players;
+    return null;
+    } 
 }
