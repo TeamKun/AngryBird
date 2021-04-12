9867b5244bb6f7953166b6eb683ed06456462b5d
Kei-7777 <69491052+Kei-7777@users.noreply.github.com>
2021-04-12 18:27:52 +0900

    修正

--- src/main/java/net/kunmc/lab/angrybird/team/TeamManager.java
+++ src/main/java/net/kunmc/lab/angrybird/team/TeamManager.java
@@ -14,50 +14,51 @@ public class TeamManager {
     List<UUID> team1_players;
     List<UUID> team2_players;
 
-    public TeamManager(){
+    public TeamManager() {
         team1_players = new ArrayList<>();
         team2_players = new ArrayList<>();
     }
 
-    public boolean isLeader(Player p){
+    public boolean isLeader(Player p) {
         return p.getUniqueId().equals(team1_leader) || p.getUniqueId().equals(team2_leader);
     }
-    
-    public boolean isExists(Player p){
-    return team1_players.contains(p.getUniqueId()) || team2_players.contains(p.getUniqueId()); 
+
+    public boolean isExists(Player p) {
+        return team1_players.contains(p.getUniqueId()) || team2_players.contains(p.getUniqueId());
     }
 
-    public boolean isAvailable(int id){
-    return id == 1 || id == 2;
-    }
-    
-    public void remove(Player p){
-    team1_players.remove(p.getUniqueId());
-    team2_players.remove(p.getUniqueId());
+    public boolean isAvailable(int id) {
+        return id == 1 || id == 2;
     }
 
-    public void add(Player p, int id){
-    if(isAvailable(id)){
-        if(getById(id).contain(p.getUniqueId()) return;
-        getById(id).add(p.getUniqueId());
-    }
-    }
-    
-    public void reset(int id){
-    if(isAvailable(id)){
-    getById(id).clear();
-    } else {
-    System.out.println("Invalid team id " + id);
+    public void remove(Player p) {
+        team1_players.remove(p.getUniqueId());
+        team2_players.remove(p.getUniqueId());
     }
 
-    public void reset(){
-    team1_players.clear();
-    team2_players.clear();
+    public void add(Player p, int id) {
+        if (isAvailable(id)) {
+            if (getById(id).contains(p.getUniqueId())) return;
+            getById(id).add(p.getUniqueId());
+        }
     }
-    
-    public List<UUID> getById(int id){
-    if(id == 1) return team1_players;
-    if(id == 2) return team2_players;
-    return null;
-    } 
+
+    public void reset(int id) {
+        if (isAvailable(id)) {
+            getById(id).clear();
+        } else {
+            System.out.println("Invalid team id " + id);
+        }
+    }
+
+    public void reset() {
+        team1_players.clear();
+        team2_players.clear();
+    }
+
+    public List<UUID> getById(int id) {
+        if (id == 1) return team1_players;
+        if (id == 2) return team2_players;
+        return null;
+    }
 }
