879fff50304a790f1727774f869081f35ede7f1d
Kei-7777 <69491052+Kei-7777@users.noreply.github.com>
2021-04-12 20:23:33 +0900

    修正v2

--- src/main/java/net/kunmc/lab/angrybird/listener/ToggleSneakEventListener.java
+++ src/main/java/net/kunmc/lab/angrybird/listener/ToggleSneakEventListener.java
@@ -1,6 +1,8 @@
 package net.kunmc.lab.angrybird.listener;
 
 import net.kunmc.lab.angrybird.Angrybird;
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
@@ -8,6 +10,7 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class ToggleSneakEventListener implements Listener {
     Angrybird ab;
+
     public ToggleSneakEventListener(Angrybird angrybird) {
         ab = angrybird;
     }
@@ -15,21 +18,21 @@ public class ToggleSneakEventListener implements Listener {
     int max = 25; // maximum power value
 
     @EventHandler
-    public void on(PlayerToggleSneakEvent e){
-        if(ab.tm.isLeader(e.getPlayer())){
-            if(e.isSneaking()){
-                new BukkitRunnable(){
-                    @Override
-                    public void run() {
-                        int lv = e.getPlayer().getLevel();
-                        if(lv <= max) lv = 0;
-                        e.getPlayer().setLevel(lv+1);
-                        if(!e.isSneaking()){
-                            cancel();
-                        }
-                    }
-                }.runTaskTimer(ab, 1,1);
+    public void on(PlayerToggleSneakEvent e) {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                int lv = e.getPlayer().getLevel();
+                if (lv >= max) lv = 0;
+                e.getPlayer().setLevel(lv + 1);
+                e.getPlayer().setExp(0);
+                e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 0.2f, lv/10);
+                if (!e.getPlayer().isSneaking()) {
+                    e.getPlayer().setLevel(0);
+                    e.getPlayer().setExp(0);
+                    cancel();
+                }
             }
-        }
+        }.runTaskTimer(ab, 1, 1);
     }
 }
